ba1e83d1c0be6775c0e87c73a5e5006da3dfef07
mykyt <mykytenko.a.g@gmail.com>
2023-12-02 20:54:55 +0200

    v0.1

---
+++ src/main/java/com/javarush/dao/CityDAO.java
@@ -0,0 +1,18 @@
+package com.javarush.dao;
+
+import com.javarush.domain.City;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+
+public class CityDAO extends GenericDAO<City> {
+    public CityDAO(SessionFactory sessionFactory) {
+        super(City.class, sessionFactory);
+    }
+
+    public City getByName(String name) {
+        Query<City> query = getCurrentSession().createQuery("select c from City c where c.city=:NAME", City.class);
+        query.setParameter("NAME", name);
+        query.setMaxResults(1);
+        return query.getSingleResult();
+    }
+}
